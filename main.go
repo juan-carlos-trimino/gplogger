fc4555cc44e429682b80b14977eb4e44a0a46118
Juan Carlos Trimino <juancarlos@trimino.com>
2024-05-18 14:13:46 -0500

    add comment.

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package logger
 
+/***
+Create repo and clone it.
+$ git clone https://github.com/juan-carlos-trimino/gplogger.git
+
+Initialize Go.
+$ cd gplogger
+Execute "go mod init github.com/{GitHub-Username}/{Repo-Name}
+$ go mod init github.com/juan-carlos-trimino/gplogger
+
+Create the file "main.go" and add the code to it.
+
+Commit and push the code.
+$ git add .
+$ git commit -m "initial commit."
+$ git push origin main
+
+Go uses "Git Tags" to manage versions of the code. Create the tag and push it.
+When code is pushed to the repo, repeat these two steps; ensure the version is changed accordingly.
+$ git tag "v1.0.0"
+$ git push origin main --tags
+
+To use the package, install it (go get -u {copy the repo url from GitHub}).
+$ go get -u github.com/juan-carlos-trimino/gplogger
+
+Next, open the file that will use the package and add this line
+("github.com/{GitHub-Username}/{Repo-Name}").
+
+import "github.com/juan-carlos-trimino/gplogger"
+
+To upgrade/downgrade the version of the package, execute
+(go get -u "{package-name}@{git-commit-hash}").
+$ go get -u "github.com/juan-carlos-trimino/gplogger@f218b1c"
+***/
+
 import (
   "encoding/json"
   "fmt"
