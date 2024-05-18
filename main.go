3709874c5f9dac04ed13e7b6588cd1d6e8999edd
Juan Carlos Trimino <juancarlos@trimino.com>
2024-05-17 19:35:37 -0500

    add json support

--- main.go
+++ main.go
@@ -1,21 +1,41 @@
 package logger
 
 import (
+  "encoding/json"
   "fmt"
   "time"
 )
 
-func LogInfo(message string) {
-  /***
-  time.Now() returns the current local time; using the current time in UTC.
-  ***/
-  fmt.Printf("%s INFO - %v", time.Now().UTC().Format(time.RFC3339Nano), message)
+type logEntry struct {
+  DateTime string "json:\"date_time\""
+  Level string `json:"level"`
+  CorrelationId string `json:"correlation_id"`
+  Message string `json:"msg"`
 }
 
-func LogWarning(message string) {
-  fmt.Printf("%s WARN - %v", time.Now().UTC().Format(time.RFC3339Nano), message)
+func LogInfo(msg, cid string) {
+  printMessage(msg, cid, "INFO")
 }
 
-func LogError(message string) {
-  fmt.Printf("%s ERROR - %v", time.Now().UTC().Format(time.RFC3339Nano), message)
+func LogWarning(msg, cid string) {
+  printMessage(msg, cid, "WARN")
+}
+
+func LogError(msg, cid string) {
+  printMessage(msg, cid, "ERROR")
+}
+
+func printMessage(msg, cid, level string) {
+  le := logEntry{
+    //time.Now() returns the current local time; using the current time in UTC.
+    DateTime: time.Now().UTC().Format(time.RFC3339Nano),
+    CorrelationId: cid,
+    Level: level,
+    Message: msg,
+  }
+  json, err := json.Marshal(le)
+  if err != nil {
+    panic(err)
+  }
+  fmt.Println(json)
 }
